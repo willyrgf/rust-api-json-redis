1ee144f2ca4d0297a2efe8f08ad9ed4344d641cd
Willy Romão <willyr.goncalves@gmail.com>
2021-01-28 21:19:17 +0000

    add a /plus/{number}/{number} route

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,12 @@ async fn main() {
     let hello = warp::path!("hello" / String)
         .map(|name| format!("Hello, {}!", name));
 
-    warp::serve(hello)
+    let plus = warp::path!("plus" / f64 / f64 )
+        .map(|n1, n2| format!("plus: {}", n1+n2));
+
+    let routes = hello.or(plus);
+
+    warp::serve(routes)
         .run(([127, 0, 0, 1], 3030))
         .await;
 
