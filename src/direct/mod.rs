c2177f697ceec54375536c00c6460c344a9aa4a5
Willy Romão <willyr.goncalves@gmail.com>
2021-02-02 20:42:15 +0000

    prepare error handlers to a direct api to redis server

---
+++ src/direct/mod.rs
@@ -0,0 +1,2 @@
+use crate::{DirectError::*, Result};
+
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-use warp::Filter;
+use warp::{Filter, Rejection};
+use thiserror::Error;
+
+mod direct;
+
+type WebResult<T> = std::result::Result<T, Rejection>;
+type Result<T> = std::result::Result<T, Error>;
 
 #[tokio::main]
 async fn main() {
@@ -14,4 +20,16 @@ async fn main() {
         .run(([127, 0, 0, 1], 3030))
         .await;
 
+}
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("direct redis error: {0}")]
+    DirectError(#[from] DirectError),
+}
+
+#[derive(Error, Debug)]
+pub enum DirectError {
+    #[error("error parsing string from redis result: {0}")]
+    RedisTypeError(redis::RedisError),
 }
